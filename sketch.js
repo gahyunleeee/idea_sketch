27b979ee756fc15fe722c842799d07cb1c8f2c43
gahyunleeee <93176757+gahyunleeee@users.noreply.github.com>
2021-12-11 17:15:30 +0900

    star
    마우스 누르면 별 떨어지도록...

--- sketch.js
+++ sketch.js
@@ -6,31 +6,19 @@ function setup() {
   for (let i = 0; i < 100; i++) {
     bugs.push(new Jitter());
   }
+  stars = new gravity();
 }
 
 function draw() {
-  background(0);
+  background(0,10);
   for (let i = 0; i < bugs.length; i++) {
     bugs[i].move();
     bugs[i].display();
   }
+
+  if (mouseIsPressed) {
+ stars.display();
+ stars.update();
+ }
+
 }
-
-// Jitter 클래스
-// class Jitter {
-//   constructor() {
-//     this.x = random(width);
-//     this.y = random(height);
-//     this.diameter = random(10, 30);
-//     this.speed = 1;
-//   }
-
-//   move() {
-//     this.x += random(-this.speed, this.speed);
-//     this.y += random(-this.speed, this.speed);
-//   }
-
-//   display() {
-//     ellipse(this.x, this.y, this.diameter, this.diameter);
-//   }
-// }
